ff39fef5d23c3c6a9c13b2d59352e2e47eb27074
Dorian Marié <dorian@dorianmarie.com>
2024-08-24 11:43:35 +0200

    add test for yaml-deep-each with tempfile

--- spec/yaml_deep_each_spec.rb
+++ spec/yaml_deep_each_spec.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "tempfile"
 
-RSpec.describe "each" do
+RSpec.describe "yaml-deep-each" do
+  it "works" do
+    file = Tempfile.create
+    File.write(file, <<~INPUT)
+      - 1
+      - 2
+      - 3
+    INPUT
+    expect(`cat #{file.path} | bin/yaml-deep-each "p it"`).to eq(<<~OUTPUT)
+    [1, 2, 3]
+    1
+    2
+    3
+    OUTPUT
+  end
 end
